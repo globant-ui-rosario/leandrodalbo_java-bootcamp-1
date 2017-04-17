783a1e8b9de2dad3fb03966e4a36e78ef3c15cbd
leandrodalbo <leadalbo@gmail.com>
2017-04-17 20:07:06 -0300

    topic0keyPoint2

---
+++ topic0KeyPoint2/src/keyPoint2/HouseBuilder.java
@@ -0,0 +1,14 @@
+package keyPoint2;
+
+public interface HouseBuilder {
+
+	public void builDoors();
+
+	public void buildWindows();
+
+	public void builDoorBell();
+
+	public House getHouse();
+
+	public void setHouse(House house);
+}
