1edf2336601dbe2c1ae357b8116b885d0b9bb712
leandrodalbo <leadalbo@gmail.com>
2017-05-10 20:01:52 -0300

    branch-cleaned

--- topic7/src/main/java/topic7/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package topic7.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import topic7.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student,Integer>{
-
-}
--- topic7/src/main/java/topic7/repositories/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package topic7.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import topic7.model.Teacher;
-
-public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
-
-}
